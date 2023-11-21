f2468868e1c18d36cd98e7f4af3e9f1e639cd2f5
Isaac-Leon1 <isaac.leon@epn.edu.ec>
2023-11-21 07:59:10 -0500

    Autos clase version 2

--- src/Autos.java
+++ src/Autos.java
@@ -1,2 +1,40 @@
 public class Autos {
+    public String marca;
+    public String modelo;
+    public String color;
+    public int anio;
+    public String cilindraje;
+
+    Autos(String nuevaMarca){
+        marca=nuevaMarca;
+    }
+
+    public String getMarca(){
+        return marca;
+    }
+    public String getModelo(){
+        return modelo;
+    }
+    public String getColor(){
+        return color;
+    }
+    public int getAnio(){
+        return anio;
+    }
+    public String getCilindraje(){
+        return cilindraje;
+    }
+
+    public void setModelo(String newModel){
+        this.modelo=newModel;
+    }
+    public void setColor(String newColor){
+        this.color=newColor;
+    }
+    public void setAnio(int newAnio){
+        this.anio=newAnio;
+    }
+    public void setCilindraje(String newCilindraje){
+        this.cilindraje=newCilindraje;
+    }
 }
