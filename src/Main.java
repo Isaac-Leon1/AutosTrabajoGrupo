5c0afcbdea31f4208ac9f023d8aa2912a1e29b26
AlejoQuiroz08 <150806260+AlejoQuiroz08@users.noreply.github.com>
2023-11-21 08:05:21 -0500

    Cambios Main

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,29 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Intro with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println("test");
+        //declarar variable tipo perros
+        Autos Auto1;
+        Autos Auto2;
+        Autos Auto3;
 
-        // Press Mayús+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        Auto1= new Autos("Toyota");
+        Auto2 = new Autos("Nissan");
+        Auto3=  new Autos("Chevrolet");
+        //Seteos
+        Auto1.setModelo("Rav4");
+        Auto2.setModelo("GTR");
+        Auto3.setModelo("Aveo");
+        Auto1.setColor("Rojo");
+        Auto2.setColor("Negro");
+        Auto3.setColor("Gris");
+        Auto1.setAnio(2003);
+        Auto2.setAnio(2010);
+        Auto3.setAnio(2021);
+        Auto1.setCilindraje("2.0L");
+        Auto2.setCilindraje("3.8L");
+        Auto3.setCilindraje("1.4L");
 
-            // Press Mayús+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        //Obtener valores de cada atributo
+
     }
 }
